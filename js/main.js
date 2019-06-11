d3c478d5a2c4fb6db49067293ac79bb748404fdd
YuriyBevov <jury19y@mail.ru>
2019-06-11 22:05:43 +0400

    Личный проект: пока все дома, использование DocumentFragment и создание отдельных функций

--- js/main.js
+++ js/main.js
@@ -18,44 +18,58 @@ var arrayPhotos = [];
 var names = ['Артем', 'Юрий', 'Ирина', 'Карина', 'Руслан'];
 
 for (var i = 1; i <= 25; i++) {
-  var url = 'photos/' + i + '.jpg';
-  var likes = getRandomInt(15, 200);
 
-  arrayPhotos.push({
-    url: url,
-    likes: likes,
-    comments: comments
-  });
+  function createPhotoArray() {
+    var url = 'photos/' + i + '.jpg';
+    var likes = getRandomInt(15, 200);
+
+    return ({url: url, likes: likes});
+  };
+
+  arrayPhotos.push(createPhotoArray());
 };
 
 for (var j = 1; j <= arrayComments.length; j++) {//количество комментариев равно длине массива с комментариями
-  var avatar = 'url:(img/' + getRandomInt(0, j) + '.svg)';//случайная аватарка
-  var name = names[getRandomInt(0, names.length-1)];//случайное имя из массива имен
-  var comment = arrayComments[getRandomInt(0, arrayComments.length-1)];//случайный комментарий
 
-  comments.push({
-    avatar: avatar,
-    name: name,
-    comment: comment
-  });
+  function createCommentArray() {
+    var avatar = 'url:(img/' + getRandomInt(0, j) + '.svg)';//случайная аватарка
+    var name = names[getRandomInt(0, names.length-1)];//случайное имя из массива имен
+    var comment = arrayComments[getRandomInt(0, arrayComments.length-1)];//случайный комментарий
+
+    return({avatar: avatar, name: name, comment: comment});
+  };
+
+  comments.push(createCommentArray());
 };
 
 //вставка элементов на страницу
-var userPicture = document.querySelector('.pictures');
 
-for (i = 0; i < arrayPhotos.length; i++) {
-  var userPhoto = document.querySelector('#picture').content.cloneNode(true);
-  var currentPhoto = arrayPhotos[i];
+var userPicture = document.querySelector('.pictures');
+var fragment = document.createDocumentFragment();
+
+function createPhotoParameters() {
+  userPhoto.querySelector('.picture__img').setAttribute('src', currentPhoto.url);
+  userPhoto.querySelector('.picture__likes').textContent = currentPhoto.likes;
+};
+
+function createPhotoComments() {
   var currentComment = [];//массив с комментариями к текущей фотографии
 
   for (var k = 0; k < getRandomInt(0, comments.length); k++) {// случайный выбор кол-ва комментариев, макс = длине comments
-    currentComment.push(comments[k]);
-  }
+   currentComment.push(comments[k]);
+  };
 
-  userPhoto.querySelector('.picture__img').setAttribute('src', currentPhoto.url);
-  userPhoto.querySelector('.picture__likes').textContent = currentPhoto.likes;
   userPhoto.querySelector('.picture__comments').textContent = currentComment.length;
-  userPicture.appendChild(userPhoto);
 };
 
-userPicture.insertAdjacentHTML('beforeend', '<div></div>')
+for (var i = 0; i < arrayPhotos.length; i++) {
+
+  var userPhoto = document.querySelector('#picture').content.cloneNode(true);
+  var currentPhoto = arrayPhotos[i];
+
+  createPhotoComments();
+  createPhotoParameters();
+  fragment.appendChild(userPhoto);
+};
+
+userPicture.appendChild(fragment);
